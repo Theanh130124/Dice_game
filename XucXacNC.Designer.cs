ddb02a10a36ca48e51ec124a2b98400639aa6027
Theanh130124 <theanhtran13012004@gmail.com~>
2024-07-02 08:07:06 +0700

    TroChoiXX

---
+++ XucXacNC.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Bai4_XucXacNC
+{
+    partial class XucXacNC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnResert = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnQuaySo = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbresult = new System.Windows.Forms.ListBox();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 227);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Quay số ";
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(235, 166);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(53, 46);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(235, 105);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(53, 46);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(235, 38);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(53, 46);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(22, 166);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(53, 46);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(22, 105);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(53, 46);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(22, 38);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(53, 46);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(103, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Silver;
+            this.groupBox2.Controls.Add(this.btnResert);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.btnQuaySo);
+            this.groupBox2.Location = new System.Drawing.Point(24, 293);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 227);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kết quả";
+            // 
+            // btnResert
+            // 
+            this.btnResert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResert.ForeColor = System.Drawing.Color.Blue;
+            this.btnResert.Location = new System.Drawing.Point(162, 168);
+            this.btnResert.Name = "btnResert";
+            this.btnResert.Size = new System.Drawing.Size(110, 40);
+            this.btnResert.TabIndex = 10;
+            this.btnResert.Text = "Resert";
+            this.btnResert.UseVisualStyleBackColor = true;
+            this.btnResert.Click += new System.EventHandler(this.btnResert_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(103, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnQuaySo
+            // 
+            this.btnQuaySo.Font = new System.Drawing.Font("Microsoft PhagsPa", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuaySo.ForeColor = System.Drawing.Color.Blue;
+            this.btnQuaySo.Location = new System.Drawing.Point(22, 167);
+            this.btnQuaySo.Name = "btnQuaySo";
+            this.btnQuaySo.Size = new System.Drawing.Size(110, 40);
+            this.btnQuaySo.TabIndex = 9;
+            this.btnQuaySo.Text = "Quay số";
+            this.btnQuaySo.UseVisualStyleBackColor = true;
+            this.btnQuaySo.Click += new System.EventHandler(this.btnQuaySo_Click_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox3.Controls.Add(this.lb3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.lb2);
+            this.groupBox3.Controls.Add(this.lb1);
+            this.groupBox3.Location = new System.Drawing.Point(24, 547);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(305, 227);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thống kê";
+            // 
+            // lb3
+            // 
+            this.lb3.AutoSize = true;
+            this.lb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb3.Location = new System.Drawing.Point(8, 176);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(57, 22);
+            this.lb3.TabIndex = 22;
+            this.lb3.Text = "Thua:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 21;
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(8, 121);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(67, 22);
+            this.lb2.TabIndex = 20;
+            this.lb2.Text = "Thắng:";
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(6, 55);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(95, 22);
+            this.lb1.TabIndex = 19;
+            this.lb1.Text = "Lượt đoán:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // lbresult
+            // 
+            this.lbresult.BackColor = System.Drawing.Color.Silver;
+            this.lbresult.FormattingEnabled = true;
+            this.lbresult.ItemHeight = 16;
+            this.lbresult.Location = new System.Drawing.Point(389, 41);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(583, 724);
+            this.lbresult.TabIndex = 18;
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSo1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtSo1.Location = new System.Drawing.Point(389, 709);
+            this.txtSo1.Multiline = true;
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(583, 56);
+            this.txtSo1.TabIndex = 19;
+            this.txtSo1.Text = "See You Again";
+            this.txtSo1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.Color.Blue;
+            this.btnThoat.Location = new System.Drawing.Point(850, 650);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(110, 40);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // XucXacNC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 804);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "XucXacNC";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.XucXacNC_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnResert;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnQuaySo;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbresult;
+        private System.Windows.Forms.Label lb3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ XucXacNC.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai4_XucXacNC
+{
+    public partial class XucXacNC : Form
+    {
+        String path;
+        int Choose;
+        int nWin, nLose, nCount;
+        public XucXacNC()
+        {
+            InitializeComponent();
+        }
+
+        private void XucXacNC_Load(object sender, EventArgs e)
+        {
+            path = Application.StartupPath + @"\HinhXucSac\";
+            Init();
+        }
+
+        private void btnResert_Click(object sender, EventArgs e)
+        {
+            Init();
+       
+        }
+
+        private void btnQuaySo_Click_1(object sender, EventArgs e)
+        {
+            Play(); // viết riêng ra như thế để cài thêm các thao tác phím cho form.
+        }
+        private void Play()
+        {
+            Random rd = new Random();
+            String kq;
+            int res = rd.Next(1, 7);
+            pictureBox2.Image = Image.FromFile(path + "XucSac" + res.ToString() + ".png");
+            nCount++;
+            if (res == Choose)
+            {
+                nWin++;
+                kq = " Thắng";
+            }
+            else
+            {
+                kq = "Thua";
+                nLose++;
+            }
+            lb1.Text = "Số lượt đã chơi là " + nCount.ToString();
+            lb2.Text = String.Format("Số lần thắng là {0} ,Tỉ lệ  {1}%", nWin, (nWin * 100) / nCount);
+            lb3.Text = String.Format("Số lần thua là {0} , Tỉ lệ {1}%", nLose, (nLose * 100) / nCount);
+            lbresult.Items.Add(String.Format("{0}.{1}(Đoán{2} ra {3})", nCount, kq, Choose, res));                        //lIST box . iteam . adđ
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtSo1.Text = txtSo1.Text.Substring(1) + txtSo1.Text.Substring(0, 1);                            // subtring(0,2) -> chỉ lấy 0 1 giống ramdom . subtring(1) -> lấy từ 1 đến hết // 
+        }
+
+        private void btn2_Click(object sender, EventArgs e)                                                    // Click nhiều button thì thằng nào đc click cuối gọi tên nó
+        {
+            Button but = (Button)sender;
+            Choose = int.Parse(but.Text);                                                                    // Text là số mấy thì nó sẽ parse qua số đó
+            pictureBox1.Image = Image.FromFile(path + "XucSac" + Choose.ToString() + ".png");
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult rel = MessageBox.Show("Bạn muốn thoát chương trình không?", " Thông báo", MessageBoxButtons.YesNo);
+            if(rel == DialogResult.Yes)
+            {
+                Close();
+            }    
+        }
+
+        private void Init()                                                                                // khi gọi thì nó sẽ khởi tạo lại hết
+        { 
+            lbresult.Items.Clear();                                                                          // Xóa hết khung result của listBox
+            lb1.Text = lb3.Text = lb2.Text = "";                                                     // Cho nó thành trống
+            Choose = 1;                                                                                     // về 1 để nó đúng với xúc xắc 1
+            pictureBox1.Image = Image.FromFile(path + "XucSac1.png");                                        //ListBox mới có . Iteam để xóa
+            nCount=nWin= nLose=0;
+            pictureBox2.Image = null;
+        }
+        protected override bool ProcessDialogKey(Keys keyData)                                            
+        {
+            //Nó trả về phím cài thêm thêm switch để sử dụng hàm . 
+            switch(keyData)
+            {
+                case Keys.Space:
+                    Play();
+                    break;
+                case Keys.Escape:
+                    Init();
+                    break;
+            }    
+            return base.ProcessDialogKey(keyData);
+        }
+        
+
+
+
+    }
+}
